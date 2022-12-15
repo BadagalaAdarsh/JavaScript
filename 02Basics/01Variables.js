77647d3ed8496c8dddbbf939557ea8890f3a8873
Adarsh Badagala <badarsh2001@gmail.com>
2022-12-16 00:07:23 +0530

    basics

---
+++ 02Basics/01Variables.js
@@ -0,0 +1,7 @@
+
+var fullName = "Adarsh Badagala" ; // semicolon is just to be consistent it is not actually required
+console.log(fullName);
+
+
+var courseName = "React JS Bootcamp" ; // data type is string
+var isLoggedIn = false; // boolean data tyupe
