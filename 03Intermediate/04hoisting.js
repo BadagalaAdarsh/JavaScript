2350858de6b6c6b47f7fc12da3505be17c3e2bec
Adarsh Badagala <badarsh2001@gmail.com>
2022-12-29 00:19:56 +0530

    4th hosting file added

--- 03Intermediate/04hoisting.js
+++ 03Intermediate/04hoisting.js
@@ -5,3 +5,19 @@ function sayMe() {
 }
 
 sayMe();
+
+function tipper(a) {
+    var bill = parseInt(a);
+    console.log(bill + 5);
+}
+
+tipper("5");
+tipper(5);
+
+function bigTipper(a) {
+    var bill = parseInt(a);
+    console.log(bill + 15);
+}
+
+bigTipper("200");
+bigTipper(200);
