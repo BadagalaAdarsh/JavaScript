8b32dc3a39c2802a1517b9884c4bec736ef7b749
Adarsh Badagala <58242963+BadagalaAdarsh@users.noreply.github.com>
2023-08-07 13:06:50 +0000

    added two files

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "badarsh2001@gmail.com"
+var accountPassword = ""
+accountCity = "Hyderabad"
+let accountState ;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+// we can also use console.log but table is little better to print multiple values at the same time
+
+/*
+Use only let
+var is a function scoped
+let is block scoped
+var is mostly hoisting based little advance for now
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // it means all the JS code as newer version of the JS
+
+// alert("hello") // since we are using node js and not browser based node so its not gonna work
+
+// use mdn and tc39 documentation for the reference
+
+let name = "adarsh";
+let age = 18;
+let isLoggedIn = false;
+
+
+// primitive data types
+// number is having a range somewhere around 2^53
+// bigint
+// string we can use either single or double quotes
+// boolean 
+// null ==> its a standalone value also a type in js (null is representation of empty, we can assign null to a variable)
+// undefined ==> 
+// symbol ==> unique 
+
+// object
+
+console.log(typeof ); // type of null is object and type of undefined is undefined
