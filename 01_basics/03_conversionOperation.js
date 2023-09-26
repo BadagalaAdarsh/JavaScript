77471bff237c0a5cbe1b345b9f4523e01cefdc03
Adarsh Badagala <58242963+BadagalaAdarsh@users.noreply.github.com>
2023-09-26 12:50:51 +0000

    added basic operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,7 +1,7 @@
 let score = true;
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 // "33" => 33
 // "33abc" => NaN (Not a Number)
@@ -13,8 +13,8 @@ let valueInNumber = Number(score); // if the score that we are passing over here
 // and if the score is undefined then the value is NaN again
 // for boolean we get 1 for true and 0 for false
 
-console.log(typeof(valueInNumber));
-console.log(valueInNumber); 
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber); 
 
 // 1 or any other number => true
 // 0 => false
@@ -25,13 +25,60 @@ let isLoggedIn = "";
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
 
-console.log(typeof booleanIsLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
 
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+
+
+
+// ************************************* Operations **********************************
+
+let value = 3;
+let negValue = -value;
+
+// console.log(negValue);
+
+// basic arithmatic operations have been skipped as it is basic
+
+let str1 = "hello"
+let str2 = " adarsh";
+
+let str3 = str1 + str2; // basic concatenation
+// console.log(str3);
+
+// console.log("1" + 2); // js infamous for concatenating as string
+// console.log(1 + "2");
+
+// console.log("1" + 2 + 2); // 122
+
+// console.log(1 + 2 + "2"); // 32 ( 1 and 2 added and later 2 was concatenated)
+// it is based on the ECMA script
+
+// if string is first everything is converted to string 
+// if string is last, then first things are converted based on priority and then later others are converted to string
+
+// console.log(+true); // just true print true but adding 
+// +true => 1
+// true+ => error
+
+// console.log(+"");
+// "" => ""
+// +"" => 0
+// ""+ => error
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++; // post fix and prefix expression
+
+console.log(gameCounter);
