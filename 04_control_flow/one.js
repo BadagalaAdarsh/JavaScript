1968a9fa941849c0d6c07ecce4da6465008f775b
Adarsh Badagala <58242963+BadagalaAdarsh@users.noreply.github.com>
2023-09-30 07:18:11 +0000

    added basic if

---
+++ 04_control_flow/one.js
@@ -0,0 +1,8 @@
+// == just type conversion and checking the value
+// ==== also checks the data type while checking
+// 2 == "2" true
+// 2 === "2" false
+// === also called strict equal
+// !== just like != but with extra = for strict
+
+// Just like a regular if else so no code written
